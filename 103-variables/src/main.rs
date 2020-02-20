b5529121ed0f6bd4e581c6d7755dbeccebaaed80
Jacek Wysocki <jacek.wysocki@gmail.com>
2020-02-20 21:52:26 +0100

    vars + types + conversion

---
+++ 103-variables/src/main.rs
@@ -0,0 +1,68 @@
+fn main() {
+    declaration();
+    mutability();
+    scope_and_shadowing();
+}
+
+fn declaration() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = ();
+
+    // copy `an_integer` into `copied_integer`
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    // The compiler warns about unused variable bindings; these warnings can
+    // be silenced by prefixing the variable name with an underscore
+    let _unused_variable = 3u32;
+}
+
+fn mutability() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error!
+    // _immutable_binding += 1;
+    // FIXME ^ Comment out this line
+}
+
+fn scope_and_shadowing() {
+    // This binding lives in the main function
+    let long_lived_binding = 1;
+
+    // This is a block, and has a smaller scope than the main function
+    {
+        // This binding only exists in this block
+        let short_lived_binding = 2;
+
+        println!("inner short: {}", short_lived_binding);
+
+        // This binding *shadows* the outer one
+        let long_lived_binding = 5_f32;
+
+        println!("inner long: {}", long_lived_binding);
+    }
+    // End of the block
+
+    // Error! `short_lived_binding` doesn't exist in this scope
+    // println!("outer short: {}", short_lived_binding);
+    // FIXME ^ Comment out this line
+
+    println!("outer long: {}", long_lived_binding);
+
+    // This binding also *shadows* the previous binding
+    let long_lived_binding = 'a';
+
+    println!("outer long: {}", long_lived_binding);
+}
---
+++ 105-conversion/src/main.rs
@@ -0,0 +1,114 @@
+use std::fmt;
+
+use std::convert::From;
+
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+fn main() {
+    from_into();
+    try_from_try_into();
+    convert_to_string();
+    parsing_a_string();
+}
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+    desc: f32,
+}
+
+// The From trait allows for a type to define how to create itself from another type,
+// hence providing a very simple mechanism for converting between several types.
+// There are numerous implementations of this trait within the standard library for
+// conversion of primitive and common types.
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number {
+            value: item,
+            desc: 0.0,
+        }
+    }
+}
+
+fn from_into() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+
+    // The Into trait is simply the reciprocal of the From trait. That is,
+    // if you have implemented the From trait for your type you get the `Into`
+    // implementation  *for free*.
+    // Try removing the type declaration
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
+
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
+    }
+}
+
+fn try_from_try_into() {
+    // TryFrom
+
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    // TryInto
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+}
+
+//Converting to String
+// To convert any type to a String is as simple as implementing the ToString
+// trait for the type. Rather than doing so directly, you should implement the
+// fmt::Display trait which automagically provides ToString and also allows
+// printing the type as discussed in the section on print!.
+
+struct Circle {
+    radius: i32,
+}
+
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Circle of radius <r={}>", self.radius)
+    }
+}
+
+fn convert_to_string() {
+    let circle = Circle { radius: 6 };
+    println!("{}", circle);
+}
+
+// One of the more common types to convert a string into is a number.
+// The idiomatic approach to this is to use the parse function and either
+// to arrange for type inference
+// or to specify the type to parse using the 'turbofish' syntax.
+// Both alternatives are shown in the following example.
+// This will convert the string into the type specified so long as the FromStr trait is implemented for that type. This is implemented for numerous types within the standard library. To obtain this functionality on a user defined type simply implement the FromStr trait for that type.
+
+fn parsing_a_string() {
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {:?}", sum);
+
+    let f: f32 = "4.534534".parse().unwrap();
+    println!("Float parse {:?}", f);
+}
